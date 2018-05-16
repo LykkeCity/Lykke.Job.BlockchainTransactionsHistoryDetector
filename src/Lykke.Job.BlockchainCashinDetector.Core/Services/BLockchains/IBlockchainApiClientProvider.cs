224f7631f9da8802fca129f2e66a5c808ad6520d
Yury Glushkov <mail@glushkov.us>
2018-05-16 17:12:28 +0300

    Redundant folders removed

--- src/Lykke.Job.BlockchainCashinDetector.Core/Services/BLockchains/IBlockchainApiClientProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lykke.Service.BlockchainApi.Client;
-
-namespace Lykke.Job.BlockchainTransactionsHistoryDetector.Core.Services.BLockchains
-{
-    public interface IBlockchainApiClientProvider
-    {
-        IBlockchainApiClient Get(string blockchainType);
-    }
-}
--- src/Lykke.Job.BlockchainCashinDetector.Core/Services/IShutdownManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Lykke.Job.BlockchainTransactionsHistoryDetector.Core.Services
-{
-    public interface IShutdownManager
-    {
-        Task StopAsync();
-    }
-}
